82a0b7234a6a8b751847c5977eb3561b2e8c8a79
Lavnik <ienvysnn@gmail.com>
2025-01-05 15:23:20 +0545

    failed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -14,6 +13,7 @@ time.sleep(1)
 
 cookie_accept= driver.find_element(By.CLASS_NAME, value="acceptAll")
 cookie_accept.click()
+# time.sleep(10)
 
 
 # ------CUSTOMIZATION-----
@@ -21,20 +21,21 @@ cookie_accept.click()
 fifty_words= driver.find_element(By.XPATH,value="/html/body/div[10]/main/div/div[1]/div/div[3]/button[2]")
 time.sleep(0.5)
 fifty_words.click()
-git
 
+#
 # infinity= driver.find_element(By.XPATH,value="//html/body/div[10]/main/div/div[1]/div/div[5]/button[5]")
+# infinity.click()
+# time.sleep(0.2)
 # infinity_input= driver.find_element(By.XPATH,value="/html/body/div[9]/dialog[2]/form/input")
 # infinity_input.send_keys("0")
-# infinity.click()
-
-
+# time.sleep(0.2)
+# ok_btn= driver.find_element(By.XPATH,value="/html/body/div[9]/dialog[2]/form/button")
+# ok_btn.click()
 # time.sleep(0.2)
 
 
 
-
-#MAIN SCRIPT
+# MAIN SCRIPT
 while True:
     word_list=[]
     words= driver.find_elements(By.ID,value="words")
@@ -47,5 +48,36 @@ while True:
     for i in range(len(separated_words)):
         text_box.send_keys(separated_words[i], Keys.SPACE)
         time.sleep(0)
+
+
+
+# -----WORK IN PROGRESSS---------
+# i=0
+# last_word_count = 0
+# while True:
+#     word_list=[]
+#     words= driver.find_elements(By.ID,value="words")
+#     print(words)
+#     text_box = driver.find_element(By.ID, 'wordsInput')
+#     for word in words:
+#             word_list.append(word.text)
+#             separated_words = word_list[0].split('\n')
+#     print(separated_words) ## this is the first iteration
+#
+#
+#     if i!=0: ## this is for the remaing iteration
+#         print(separated_words[:last_word_count])
+#         del separated_words[:last_word_count]
+#
+#     for i in range(len(separated_words)):
+#         text_box.send_keys(separated_words[i], Keys.SPACE)
+#         time.sleep(0.01)
+#     last_word_count = len(separated_words) + last_word_count
+#     i+=1
+#     time.sleep(0.1)
+#
+
+
+
 
 
